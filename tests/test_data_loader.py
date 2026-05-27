680041d6605905e149e051dd9c39eac042441645
Yengusie Demilie <yengusied@gmail.com>
2026-05-27 12:25:51 +0300

    feat: Create and Run  Unit Tests

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,8 +1,14 @@
-
 import pandas as pd
+
+# Define dataset path
+file_path = "data/MachineLearningRating_v3_cleaned.csv"
+
 def test_load_data():
     # Load dataset
     df = pd.read_csv(file_path)
-    df = load_data("data/machine_learning_data.csv")
-    missing = df.isnull().sum().sort_values(ascending=False)
-    missing[missing > 0]
+
+    # Ensure dataset is not empty
+    assert not df.empty
+
+    # Ensure dataframe exists
+    assert df is not None
